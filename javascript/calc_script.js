9e875af62d84ebd66e0dea152bbc02f740931f0d
Naureen <naureen.imran123@yahoo.com>
2022-11-07 13:57:08 +0000

    create calculator class

--- javascript/calc_script.js
+++ javascript/calc_script.js
@@ -1,8 +1,24 @@
-const numberButtons = document.querySelectorAll(['data-number']);
-const operatorButtons = document.querySelectorAll(['data-operator']);
-const clearButton = document.querySelector(['data-all-clear']);
-const delButton = document.querySelector(['data-delete']);
-const equalButton = document.querySelector(['data-equal']);
+class Calculator {
+    constructor(previousOperandTextElement, currantOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currantOperandTextElement = currantOperandTextElement;
+        this.clear();
+    }
+    clear() {
+        this.currantOperand = '';
+        this.previousOperand = '';
+        this.operator = undefined;
+    }
+};
 
-const previousOperandTextElement = document.querySelector(['data-previous-operand']);
-const currantOperandTextElement = document.querySelector(['data-currant-operand']);
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-all-clear]');
+const delButton = document.querySelector('[data-delete]');
+const equalButton = document.querySelector('[data-equal]');
+
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currantOperandTextElement = document.querySelector('[data-currant-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currantOperandTextElement);
+console.log(calculator);
