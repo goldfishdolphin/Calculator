5d8e6b48c8685f2c0eea68825538b746cd77e4fb
Naureen <naureen.imran123@yahoo.com>
2022-11-07 12:23:34 +0000

    make variables for buttons and text element

---
+++ javascript/calc_script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll(['data-number']);
+const operatorButtons = document.querySelectorAll(['data-operator']);
+const clearButton = document.querySelector(['data-all-clear']);
+const delButton = document.querySelector(['data-delete']);
+const equalButton = document.querySelector(['data-equal']);
+
+const previousOperandTextElement = document.querySelector(['data-previous-operand']);
+const currantOperandTextElement = document.querySelector(['data-currant-operand']);
